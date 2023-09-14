14419c06724752d931485edb70e5dea30a406ee8
NgocNhuNguyenThi <nhu02349@gmail.com>
2023-09-14 14:50:14 +1000

    Final documents

--- climate_q1.py
+++ climate_q1.py
@@ -5,6 +5,21 @@ years = []
 co2 = []
 temp = []
 
+# Define connection and cursor
+connection = sqlite3.connect("climate.db")
+cursor = connection.cursor()
+
+# Fetch values from database
+cursor.execute("SELECT * FROM ClimateData")
+results = cursor.fetchall()
+
+# Populate Python lists with corresponding values
+for record in results:
+    years.append(record[0])
+    co2.append(record[1])
+    temp.append(record[2])
+
+
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
@@ -17,19 +32,4 @@ plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)") 
 plt.show() 
 plt.savefig("co2_temp_1.png") 
-
-# Define connection and cursor
-connection = sqlite3.connect("climate.db")
-cursor = connection.cursor()
-
-# Fetch values from database
-cursor.execute("SELECT * FROM ClimateData")
-results = cursor.fetchall()
-print(results)
-
-# Populate Python lists with corresponding values
-for record in results:
-    years.append(record[0])
-    co2.append(record[1])
-    temp.append(record[2])
 
--- climate_q2.py
+++ climate_q2.py
@@ -5,6 +5,14 @@ years = []
 co2 = []
 temp = []
 
+# Load the data from the CSV file
+df = pd.read_csv(".\climate.csv")
+
+# Show the data in figure
+years = df["Year"].tolist()
+co2 = df["CO2"].tolist()
+temp = df["Temperature"].tolist()
+
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
@@ -17,4 +25,5 @@ plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)") 
 plt.show() 
 plt.savefig("co2_temp_2.png") 
+
 
